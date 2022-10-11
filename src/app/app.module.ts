27c301bff9e6e09f5cfc171c4201b7f366f7d552
Ann-Annice <annannice98@gmail.com>
2022-10-11 14:55:31 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosappComponent } from './photosapp/photosapp.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotosappComponent
+    PhotosappComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
